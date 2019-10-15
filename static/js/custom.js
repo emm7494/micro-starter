a37bacec560b94e9767abaa093e64ccc532612a5
emm7494 <emm7494@gmail.com>
2019-10-15 01:58:42 +0000

    Implemented intersection observer API and added drop shadow to navbar

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,41 @@
+const heroSentinel = document.createElement("div");
+heroSentinel.classList.add("hero_sentinel");
+
+const hero = document.querySelector(".hero");
+hero.appendChild(heroSentinel);
+
+const siteNavbar = document.querySelector(".site-navbar");
+
+const io = new IntersectionObserver(
+  (entries, observer) => {
+    console.log(observer.rootMargin);
+    entries.forEach((entry) => {
+      console.log(
+        entry.target,
+        entry.intersectionRatio,
+        entry.intersectionRect,
+        entry.boundingClientRect,
+        entry.rootBounds,
+        entry.isIntersecting
+      );
+
+      ["navbar-dark"].forEach((cls) =>
+        siteNavbar.classList.toggle(cls, entry.isIntersecting)
+      );
+      ["navbar-light", "bg-light", "site-navbar--shadow"].forEach((cls) =>
+        siteNavbar.classList.toggle(cls, !entry.isIntersecting)
+      );
+      if (!entry.isIntersecting) {
+        console.log("not intersecting");
+      } else {
+        console.log("is intersecting");
+      }
+    });
+  },
+  { root: null, rootMargin: `0px 0px 0px`, threshold: [0] }
+);
+
+io.observe(heroSentinel);
 // $(".main-site-nav .nav-link").on("click", function() {
 //   $(this)
 //     .css({ "color": "green" })
@@ -40,20 +78,20 @@
 //   oldScrollPos = newScrollPos;
 // };
 
-var ele = document.querySelector("#on-test");
-var io = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      console.log(entry.target, observer);
-    });
-  },
-  {
-    // delay: 100,
-    // trackVisibility: true
-  }
-);
+// var ele = document.querySelector("#on-test")[0];
+// var io = new IntersectionObserver(
+//   (entries, observer) => {
+//     entries.forEach((entry) => {
+//       console.log(entry.target, observer);
+//     });
+//   },
+//   {
+//     // delay: 100,
+//     // trackVisibility: true
+//   }
+// );
 
-io.observe(ele);
+// io.observe(ele);
 
 // var prevScrollpos = window.pageYOffset;
 // window.onscroll = function () {
